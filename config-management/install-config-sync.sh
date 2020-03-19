6863814253db0c4c0233102eb6983211c816d43a
Guillermina Muro <guillermina.muro@gmail.com>
2020-03-19 16:19:55 -0700

    updated dir name

--- config-management/install-config-sync.sh
+++ config-management/install-config-sync.sh
@@ -19,7 +19,7 @@ export CLUSTER_NAME=$(kubectl config current-context)
 export BASE_DIR=${BASE_DIR:="${PWD}/.."}
 echo "BASE_DIR set to $BASE_DIR"
 
-source $BASE_DIR/2-ConfigManagement/config-repo-url.env
+source $BASE_DIR/config-management/config-repo-url.env
 
 echo "### "
 echo "### Begin install config sync"
@@ -29,7 +29,7 @@ echo "### "
 
 
 ## Poll the Config Repository
-(set -x; cat $BASE_DIR/2-ConfigManagement/config_sync.yaml | sed 's@<REPO_URL>@'${REPO_URL}@g | sed 's@<CLUSTER_NAME>@'${CLUSTER_NAME}@g | kubectl apply -f -)
+(set -x; cat $BASE_DIR/config-management/config_sync.yaml | sed 's@<REPO_URL>@'${REPO_URL}@g | sed 's@<CLUSTER_NAME>@'${CLUSTER_NAME}@g | kubectl apply -f -)
 
 
 
